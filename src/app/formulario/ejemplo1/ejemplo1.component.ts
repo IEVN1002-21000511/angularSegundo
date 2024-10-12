05b11d8217854ac2015fe4de6d1f01d259144df5
Jank otzumi <21000511@alumnos.utleon.edu.mx>
2024-10-12 10:26:34 -0600

    Rutas

--- src/app/formulario/ejemplo1/ejemplo1.component.ts
+++ src/app/formulario/ejemplo1/ejemplo1.component.ts
@@ -1,3 +1,4 @@
+import { JsonPipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder,FormControl,FormGroup, ReactiveFormsModule } from '@angular/forms';
 
@@ -17,6 +18,14 @@ interface Usuarios{
 export class Ejemplo1Component implements OnInit{
   formGroup!:FormGroup;
 
+  materia='pwa'
+  tem=''
+  alumnos:Usuarios={
+    nombre:'',
+    edad:0,
+    email:''
+  }
+
   constructor(private fb:FormBuilder){ }
 
 
@@ -31,7 +40,25 @@ export class Ejemplo1Component implements OnInit{
     })
   }
   onSubmit():void{
+    const{nombre,edad,email}=this.formGroup.value   /* desestructuracion de datos */
+    this.alumnos.nombre=nombre;
+    this.alumnos.edad=edad;
+    this.alumnos.email=email;
+    let alumnoJSON=JSON.stringify(this.alumnos);
+
     console.log(this.formGroup.value);
+
+    localStorage.setItem('materia',this.materia);
+    localStorage.setItem('alumno',alumnoJSON);
+  }
+
+  subImprimir():void{
+    this.tem=localStorage.getItem('materia')!
+
+    const alumnoGuardado=localStorage.getItem('alumno')
+    if(alumnoGuardado){
+      const alumno:Usuarios=JSON.parse(alumnoGuardado)
+    }
   }
 
 }
